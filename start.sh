1e95b8d73d197664b9c23a07daa2c58689c4a972
Theriley106 <christopherlambert106@gmail.com>
2019-07-16 17:14:40 -0400

    Added start script

---
+++ start.sh
@@ -0,0 +1,20 @@
+intexit() {
+    # Kill all subprocesses (all processes in the current process group)
+    kill -HUP -$$
+}
+
+hupexit() {
+    # HUP'd (probably by intexit)
+    echo
+    echo "Interrupted"
+    exit
+}
+
+trap hupexit HUP
+trap intexit INT
+
+./mitmdump --no-http2 -p 8084 -s ./mitmProxyTest.py &
+./app.py &
+./browser.py &
+
+wait
